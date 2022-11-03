7b64835c4a8d6b0ed7c4c7d0a144012b25de64c8
vasily <vaslobas@gmail.com>
2022-11-03 14:44:07 +0300

    Add games rules screen and linked it up to button on main screen

--- millionaire/Controller/ViewController.swift
+++ millionaire/Controller/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gameRullesButton(_ sender: Any) {
+        let gameRulesVC = GameRulesVC()
+        self.present(gameRulesVC, animated: true)
+    }
+    
 }
 
