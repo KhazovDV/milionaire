dd5455c9162ffff7576a4432fd81b23a833a8130
vasily <vaslobas@gmail.com>
2022-11-03 15:26:45 +0300

    add extension to UIColor to use hex codes for colours definition

--- millionaire/Controller/GameRulesVC.swift
+++ millionaire/Controller/GameRulesVC.swift
@@ -1,13 +1,32 @@
 import UIKit
 
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
+
 class GameRulesVC: UIViewController {
 
     var scrollView = UIScrollView()
     var label = UILabel()
     var button = UIButton()
     
-    let backgroundColor = UIColor(red: 0.0/255.0, green: 173/255, blue: 181/255, alpha: 1.0)
-    let textColor = UIColor(red: 238, green: 238, blue: 238, alpha: 1)
+    let backgroundColor = UIColor(rgb: 0x00ADB5)
+    let textColor = UIColor(rgb: 0xEEEEEE)
     
 
     override func viewDidLoad() {
