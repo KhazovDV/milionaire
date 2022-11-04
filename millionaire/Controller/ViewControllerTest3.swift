0b26fe8e645fee6d3c920914ccba97f221c749a8
KhazovDV <garpun961@gmail.com>
2022-11-04 20:17:15 +0300

    Add baton in ViewControllerTest3

---
+++ millionaire/Controller/ViewControllerTest3.swift
@@ -0,0 +1,37 @@
+//
+//  ViewControllerTest3.swift
+//  millionaire
+//
+//  Created by garpun on 04.11.2022.
+//
+
+import UIKit
+
+class ViewControllerTest3: UIViewController {
+
+    @IBOutlet weak var questionsLabel: UITextView!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func HintFiftyButton(_ sender: UIButton) {
+    }
+    @IBAction func HintSeventeeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func HintImmortalityButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func HintTakeMoneyButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func Answers(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
