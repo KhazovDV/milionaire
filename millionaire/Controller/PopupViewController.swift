8c5aa855d8a542434eaae5cb157a7b727b4c4d54
vasily <vaslobas@gmail.com>
2022-11-05 11:13:46 +0300

    clean code popupview

--- millionaire/Controller/PopupViewController.swift
+++ millionaire/Controller/PopupViewController.swift
@@ -9,34 +9,10 @@ class PopupViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        popupView.layer.cornerRadius = 16
         view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
-        moveIn()
         
+        popupView.layer.cornerRadius = 16
     }
-
-    //@IBAction func closePressed(_ sender: UIButton) {
-  //      moveOut()
-  //  }
-
-
-    func moveIn() {
-        self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
-        self.view.alpha = 0.0
-
-        UIView.animate(withDuration: 0.24) {
-            self.view.transform = CGAffineTransform.identity
-            self.view.alpha = 1.0
-        }
-    }
-
-    func moveOut() {
-        UIView.animate(withDuration: 0.24, animations: {
-            self.view.transform = CGAffineTransform(scaleX: 1.35, y: 1.35)
-            self.view.alpha = 0.0
-        }) { _ in
-            self.view.removeFromSuperview()
-        }
-    }
+         
 
 }
